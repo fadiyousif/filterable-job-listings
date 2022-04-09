8500be1042bae49e6e6cfcd4a279ee0f621072b5
Fadi <fadi_bassam@hotmail.com>
2022-04-09 16:50:52 +0200

    create Image component

---
+++ src/components/Image.tsx
@@ -0,0 +1,9 @@
+type ImageProps = {
+   src: string
+   alt: string
+   className?: string
+}
+
+export const Image = ({ src, alt, className }: ImageProps) => (
+   <img src={src} alt={alt} className={className} />
+)
